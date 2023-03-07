736d248f0c37b68f75e51685ec2235fc8130612b
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2023-03-07 01:06:17 +0000

    Solution branch added

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 // we declare a new global variable containing an array that represents the ballons map
+const color = ["green", "red", "violet", "black", "yellow", "purple", "grey", "gray", "brown", "blue", "pink", "orange"];
+
 // you have to add more colors into the ballonsMap array
-let ballonsMap = ['green'];
-
-// poping a balloon is basically turning his color to null (no color)
-const popBalloon = (position) => {
-    // set the color to null on the balloon position
-    render();
-}
-
-const render = () => {
+let ballonsAlive = 20;
+//generando de forma dinamica los colores
+for (let i=0; i<20; i++){
+    const randomColor = color[Math.floor(Math.random()*color.length)]
+    let ballon = document.createElement("div");
+    ballon.classList.add('balloon', 'balloon');
+    ballon.style.background = randomColor;
+    ballon.id = i;
     
-    // convert ballons map of colors into real html balloons
-    const ballons = ballonsMap.map((color, position) => {
-        return `<div class="balloon active"></div>`; // <--- render each balloon
+    // Agregar EventListener a cada ballon:
+    ballon.addEventListener('click', (e) => {
+        ballon.style.visibility = 'hidden';
+        ballonsAlive--;
     });
 
-    document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
-    document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
+    // Agregar cada ballon a la lista de ballons:
+    document.querySelector("#balloon-map").appendChild(ballon);
+  }
 
-    if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
+const render = () => {
+    document.querySelector("#balloon-count").innerHTML = ballonsAlive; // <-- render the balloon count into the DOM
+   
+    if(ballonsAlive == 0) window.location.reload(); // <--- reload website when no more balloons are left
 }
 
 // this makes the "render" function trigger when the website starts existing
